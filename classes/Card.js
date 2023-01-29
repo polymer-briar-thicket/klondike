6e5a57345fb4927aa6d3feff744bfea419d0ac31
polymer-briar-thicket <117550910+polymer-briar-thicket@users.noreply.github.com>
2023-01-29 03:42:46 -0700

    groundwork for cards in dom

--- classes/Card.js
+++ classes/Card.js
@@ -4,25 +4,53 @@ export default class Card {
   constructor(suit, value) {
     this.suit = suit;
     this.value = value;
+    this.is_face_up = true;
 
     this.dom = {
-      "border": document.createElement("div"),
+      "card": document.createElement("div"),
       "index_value": document.createElement("div"),
       "index_suit": document.createElement("div"),
-      "suit": document.createElement("div")
+      "pip": document.createElement("div")
     };
+
+    this.dom.card.classList.add("card");
+    this.dom.card.style.borderColor = this.suit.color.hsl_string;
+    this.dom.card.style.color = this.suit.color.hsl_string;
+
+    this.dom.index_value.classList.add("index-value");
+    this.dom.index_value.innerHTML = this.value.symbol;
+
+    this.dom.index_suit.classList.add("index-suit");
+    this.dom.index_suit.innerHTML = this.suit.symbol;
+
+    this.dom.pip.classList.add("pip");
+    this.dom.pip.innerHTML = this.suit.symbol;
+
+    this.dom.card.appendChild(this.dom.index_value);
+    this.dom.card.appendChild(this.dom.index_suit);
+    this.dom.card.appendChild(this.dom.pip);
+
+    document.body.appendChild(this.dom.card);
+  }
+
+  get name() {
+    return this.value.name.singular + " of " + this.suit.name.plural;
+  }
+
+  get is_face_down() {
+    !this.is_face_up;
   }
 
   get position() {
     return new Position3(
-      this.dom.border.style.left,
-      this.dom.border.style.top,
-      this.dom.border.style.zIndex
+      this.dom.card.style.left,
+      this.dom.card.style.top,
+      this.dom.card.style.zIndex
     );
   }
   set position(position3) {
-    if (position3.x != null) this.dom.border.style.left = position3.x;
-    if (position3.y != null) this.dom.border.style.top = position3.y;
-    if (position3.x != null) this.dom.border.style.zIndex = position3.z;
+    if (position3.x != null) this.dom.card.style.left = position3.x;
+    if (position3.y != null) this.dom.card.style.top = position3.y;
+    if (position3.x != null) this.dom.card.style.zIndex = position3.z;
   }
 }
--- classes/Suit.js
+++ classes/Suit.js
@@ -1,10 +1,10 @@
 export default class Suit {
-  constructor(singular, plural, symbol_solid, symbol_outline, color) {
+  constructor(singular, plural, symbol, symbol_outline, color) {
     this.name = {
       "singular": singular,
       "plural": plural
     };
-    this.symbol_solid = symbol_solid;
+    this.symbol = symbol;
     this.symbol_outline = symbol_outline;
     this.color = color;
   }
