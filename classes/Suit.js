cb2e4927a3792549c3bb3c1e1b6558df28ef8b7f
polymer-briar-thicket <117550910+polymer-briar-thicket@users.noreply.github.com>
2023-01-29 02:51:03 -0700

    Update Suit.js

--- classes/Suit.js
+++ classes/Suit.js
@@ -1,7 +1,9 @@
 export default class Suit {
   constructor(singular, plural, symbol_solid, symbol_outline, color) {
-    this.name.singular = singular;
-    this.name.plural = plural;
+    this.name = {
+      "singular": singular,
+      "plural": plural
+    }
     this.symbol_solid = symbol_solid;
     this.symbol_outline = symbol_outline;
     this.color = color;
