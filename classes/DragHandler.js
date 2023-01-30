839c2cd7d5889766f696fbaaa48ff13ca614db4e
polymer-briar-thicket <117550910+polymer-briar-thicket@users.noreply.github.com>
2023-01-30 01:38:38 -0700

    Update DragHandler.js

--- classes/DragHandler.js
+++ classes/DragHandler.js
@@ -6,6 +6,9 @@ export default class DragHandler {
       "y": 0
     };
     this.element.addEventListener("touchstart", this.TouchStart.bind(this));
+
+    this.touchmove = this.TouchMove.bind(this);
+    this.touchend = this.TouchEnd.bind(this);
   }
 
   TouchStart(event) {
@@ -14,18 +17,18 @@ export default class DragHandler {
     this.offset.y = event.targetTouches[0].clientY - this.element.getBoundingClientRect().top;
     console.log("offset.x: " + this.offset.x + " | offset.y: " + this.offset.y);
 
-    this.element.addEventListener("touchmove", this.TouchMove.bind(this));
-    this.element.addEventListener("touchend", this.TouchEnd.bind(this));
-    this.element.addEventListener("touchcancel", this.TouchEnd.bind(this));
+    this.element.addEventListener("touchmove", this.touchmove);
+    this.element.addEventListener("touchend", this.touchend);
+    this.element.addEventListener("touchcancel", this.touchend);
   }
   TouchMove(event) {
     console.log(event.targetTouches[0].screenX - this.offset.x);
-    this.element.style.left = event.targetTouches[0].clientX - this.offset.x;
-    this.element.style.top = event.targetTouches[0].clientY - this.offset.y;
+    this.element.style.left = (event.targetTouches[0].clientX - this.offset.x) + "px";
+    this.element.style.top = (event.targetTouches[0].clientY - this.offset.y) + "px";
   }
   TouchEnd() {
-    this.element.removeEventListener("touchmove", this.TouchMove);
-    this.element.removeEventListener("touchend", this.TouchEnd);
-    this.element.removeEventListener("touchcancel", this.TouchEnd);
+    this.element.removeEventListener("touchmove", this.touchmove);
+    this.element.removeEventListener("touchend", this.touchend);
+    this.element.removeEventListener("touchcancel", this.touchend);
   }
 }
