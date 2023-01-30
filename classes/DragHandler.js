f2348033c25577362065de069f119f58b166d222
polymer-briar-thicket <117550910+polymer-briar-thicket@users.noreply.github.com>
2023-01-30 01:27:51 -0700

    Update DragHandler.js

--- classes/DragHandler.js
+++ classes/DragHandler.js
@@ -14,12 +14,12 @@ export default class DragHandler {
     this.offset.y = event.targetTouches[0].clientY - this.element.getBoundingClientRect().top;
     console.log("offset.x: " + this.offset.x + " | offset.y: " + this.offset.y);
 
-    this.element.removeEventListener("touchstart", this.TouchStart);
     this.element.addEventListener("touchmove", this.TouchMove.bind(this));
     this.element.addEventListener("touchend", this.TouchEnd.bind(this));
     this.element.addEventListener("touchcancel", this.TouchEnd.bind(this));
   }
   TouchMove(event) {
+    console.log(event.targetTouches[0].screenX - this.offset.x);
     this.element.style.left = event.targetTouches[0].screenX - this.offset.x;
     this.element.style.top = event.targetTouches[0].screenY - this.offset.y;
   }
@@ -27,6 +27,5 @@ export default class DragHandler {
     this.element.removeEventListener("touchmove", this.TouchMove);
     this.element.removeEventListener("touchend", this.TouchEnd);
     this.element.removeEventListener("touchcancel", this.TouchEnd);
-    this.element.addEventListener("touchstart", this.TouchStart.bind(this));
   }
 }
